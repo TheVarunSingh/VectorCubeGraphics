15cc74427a93e8aec36c2da4e45e4d2f0045d9a1
Varun Singh <vsingh@hmc.edu>
2020-11-29 13:16:10 -0800

    Fixed homogenous coordinates subtraction

--- lib/transformations/cubetransformations.h
+++ lib/transformations/cubetransformations.h
@@ -13,7 +13,7 @@ static const int BACK_LEFT_DOWN_IDX    = 5;
 static const int BACK_RIGHT_DOWN_IDX   = 6;
 static const int BACK_RIGHT_UP_IDX     = 7;
 
-static const float origin[4] = {0, 0, 0, 0};
+static const float origin[4] = {0, 0, 0, 1};
 
 static const float cubeVertices[8][4] = {
     { 1,  1,  1,  1},
