d1d4b1836adcb898a3c0199e1540c1d7f6ce8db4
Varun Singh <vsingh@hmc.edu>
2020-11-29 18:44:11 -0800

    One cross-platform keyboard input program

---
+++ keyboard_input.py
@@ -0,0 +1,62 @@
+import serial
+from sys import platform
+
+if "win32" in platform:
+    import msvcrt
+else:
+    import getch
+
+VALID_CHARS = "wasdqeijkluovn"
+QUIT_CHARS = "b"
+
+def openSerialPort():
+    serialPort = serial.Serial(
+        port='COM7',
+        baudrate=115200,
+        bytesize=serial.EIGHTBITS,
+        parity=serial.PARITY_NONE,
+        stopbits=serial.STOPBITS_ONE,
+        write_timeout=1
+    )
+    print("Opening serial port", serialPort.name)
+    return serialPort
+
+def windowsLoop(serialPort):
+    while True:
+        if msvcrt.kbhit():
+            char = msvcrt.getch().decode('utf-8')
+
+            if char in VALID_CHARS:
+                print("You pressed:", char)
+                serialPort.write(char.encode('utf-8'))
+
+            if char in QUIT_CHARS:
+                print("Bye bye! See you next time!")
+                print("Quitting and closing serial port", serialPort.name)
+                serialPort.close()
+                break
+
+def linuxLoop(serialPort):
+    while True:
+        char = getch.getch()
+
+        if char in VALID_CHARS:
+            print("You pressed:", char)
+            serialPort.write(char.encode('utf-8'))
+
+        if char in QUIT_CHARS:
+            print("Bye bye! See you next time!")
+            print("Quitting and closing serial port", serialPort.name)
+            serialPort.close()
+            break
+
+def main():
+    serialPort = openSerialPort()
+
+    if "win32" in platform:
+        windowsLoop(serialPort)
+    else:
+        linuxLoop(serialPort)
+
+if __name__ == "__main__":
+    main()
